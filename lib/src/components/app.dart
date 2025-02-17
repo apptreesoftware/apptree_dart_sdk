d25e6783debbb4586b65bcc7dff2ac647a50bdcb
aicorey <corey@apptreesoftware.com>
2025-02-17 15:34:11 -0800

    Copy and Output Templates

--- lib/src/components/app.dart
+++ lib/src/components/app.dart
@@ -3,13 +3,14 @@ import "package:apptree_dart_sdk/src/components/feature.dart";
 import "package:apptree_dart_sdk/src/components/menu.dart";
 import "package:yaml_writer/yaml_writer.dart";
 import "package:apptree_dart_sdk/src/util/file.dart";
+
 class App {
   final String name;
   final String version;
   List<Builder> builders = [];
   List<Feature> features = [];
   List<MenuItem> menuItems = [];
-
+  List<String> layouts = [];
   App({required this.name, required this.version});
 
   void addFeature(Builder builder, MenuItem? menuItem) {
@@ -18,14 +19,6 @@ class App {
       menuItem.setId(builder.id);
       menuItems.add(menuItem);
     }
-  }
-
-  Map<String, dynamic> toDict() {
-    return {"name": name, "version": version, "merge": [], "layouts": []};
-  }
-
-  String toYaml() {
-    return YAMLWriter().write(toDict());
   }
 
   void buildFeature(Builder builder) {
@@ -41,6 +34,21 @@ class App {
     features.add(feature);
   }
 
+  Map<String, dynamic> toDict() {
+    return {
+      "name": name,
+      "version": version,
+      "merge": features.map((feature) => '${feature.id}.yaml').toList(),
+      "layouts": layouts
+    };
+  }
+
+  void toYaml() {
+    Map<String, dynamic> app = toDict();
+
+    writeYaml(name, "template_merge.apptreemobile", YAMLWriter().write(app));
+  }
+
   void initialize() {
     for (var builder in builders) {
       buildFeature(builder);
@@ -54,5 +62,9 @@ class App {
     for (var feature in features) {
       writeYaml(name, feature.id, feature.toYaml());
     }
+
+    layouts = copyTemplates(name);
+
+    toYaml();
   }
 }
--- lib/src/util/file.dart
+++ lib/src/util/file.dart
@@ -13,3 +13,31 @@ class FileUtil {
 void writeYaml(String dir, String fileName, String yaml) {
   FileUtil.writeYaml(dir, fileName, yaml);
 }
+
+List<String> copyDirectory(Directory source, Directory destination) {
+  if (!destination.existsSync()) {
+    destination.createSync(recursive: true);
+  }
+
+  for (FileSystemEntity entity in source.listSync(recursive: false)) {
+    String newPath = '${destination.path}/${entity.uri.pathSegments.last}';
+
+    if (entity is File) {
+      entity.copySync(newPath);
+    } else if (entity is Directory) {
+      copyDirectory(entity, Directory(newPath));
+    }
+  }
+
+  // Returns a list of the filenames prefixed with templates/
+  return destination.listSync().map((e) => 'templates/${e.uri.pathSegments.last}').toList();
+}
+
+List<String> copyTemplates(String dir) {
+  Directory source = Directory('res/includes/templates');
+  Directory destination = Directory('res/$dir/templates');
+  if (!destination.existsSync()) {
+    destination.createSync(recursive: true);
+  }
+  return copyDirectory(source, destination);
+}
