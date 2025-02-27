64b63d1edf58ce392d4e3ac889ea51c89a10c931
aicorey <corey@apptreesoftware.com>
2025-02-27 15:45:07 -0800

    Fixing Rebase Nightmare

--- lib/src/components/template.dart
+++ lib/src/components/template.dart
@@ -2,11 +2,9 @@ import 'package:apptree_dart_sdk/apptree.dart';
 import 'package:apptree_dart_sdk/apptree.dart';
 
 abstract class Template {
-abstract class Template {
   final String id;
   bool isFormTemplate = false;
 
-  Template({required this.id});
   Template({required this.id});
 
   void setIsFormTemplate(bool isFormTemplate) {
@@ -26,44 +24,7 @@ class Workbench extends Template {
 
   @override
   Map<String, dynamic> toDict() => {
-  Map<String, dynamic> toDict();
-
-  String toFsx();
-}
-
-class Workbench extends Template {
-  final StringField title;
-  final StringField subtitle;
-
-  Workbench({required this.title, required this.subtitle}) : super(id: "workbench");
-
-  @override
-  Map<String, dynamic> toDict() => {
         'id': id,
-        'values': {
-          'title': title,
-          'subtitle': subtitle,
-        },
-      };
-
-  @override
-  String toFsx() => '''
-<template id="$id">
-  <Card elevation="1" color="#ffffff">
-    <Padding padding="8.0,8.0,8.0,8.0">
-      <Column mainAxisSize="min">
-        // Title
-        <Row crossAxisAlignment="center" mainAxisAlignment="spaceBetween" mainAxisSize="max">
-            <Text textAlign="start" overflow="ellipsis" style:fontColor="ff448aff" style:fontSize="18.0"
-                  value:binding="title" value="This is the title"/>
-        </Row>
-        // Subtitle
-        <Text textAlign="start" value:binding="subtitle" value="This is the subtitle"/>
-      </Column>
-    </Padding>
-  </Card>
-</template>
-  ''';
         'values': {
           'title': title,
           'subtitle': subtitle,
