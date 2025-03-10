b92aae6a1464e8fbdce1774e69a0ddac84ba632b
aicorey <corey@apptreesoftware.com>
2025-03-10 12:33:25 -0700

    Cleaning up problems

--- lib/src/generators/sample_generator.dart
+++ lib/src/generators/sample_generator.dart
@@ -1,9 +1,8 @@
-import 'package:langchain/langchain.dart';
-import 'package:langchain_openai/langchain_openai.dart';
 import 'package:apptree_dart_sdk/src/util/file.dart';
 import 'package:apptree_dart_sdk/src/util/strings.dart';
 import 'package:apptree_dart_sdk/apptree.dart';
 import 'dart:mirrors';
+import 'package:apptree_dart_sdk/src/util/llm/langchain.dart';
 
 abstract class SampleGenerator {
   final Record record;
@@ -45,78 +44,6 @@ abstract class SampleGenerator {
   Future<String> generateSampleClass();
 
   Future<void> generateSamples();
-
-  Future<String> _generateSampleDataLLM(String codeRequest) async {
-    // 1. Attempt to read in existing samples
-    final existingSamples = await readSamplePartialDart(
-      projectDir,
-      getFileName(),
-    );
-    if (existingSamples.isNotEmpty && overwrite) {
-      return existingSamples;
-    }
-
-    // 2. Generate new samples
-    final vectorStore = MemoryVectorStore(
-      embeddings: OpenAIEmbeddings(apiKey: openaiApiKey),
-    );
-    final modelDart = await readModelDart(projectDir, getRecordName());
-    await vectorStore.addDocuments(
-      documents: [Document(pageContent: modelDart)],
-    );
-
-    // 3. Define the retrieval chain
-    final retriever = vectorStore.asRetriever();
-    final setupAndRetrieval = Runnable.fromMap<String>({
-      'context': retriever.pipe(
-        Runnable.mapInput((docs) => docs.map((d) => d.pageContent).join('\n')),
-      ),
-      'code_request': Runnable.passthrough(),
-    });
-
-    const String systemPrompt = '''
-        You are a Dart developer.
-        You are given a code request and a context.
-        You need to generate a Dart code snippet that implements the code request.
-        The code should be formatted using Dart's formatting rules.
-        The code should be a string representation of the objects in Dart declared with the name 'samples' as a list of the objects.
-        Do not include any imports.
-        This will be declared as a static variable in the class.
-        Any string fields should be wrapped in double quotes.
-        Output in JSON format with the following keys:
-        - prefix: A brief explanation of the code
-        - code: The Dart code snippet
-        {context}
-      ''';
-
-    // 4. Construct a RAG prompt template
-    final promptTemplate = ChatPromptTemplate.fromPromptMessages([
-      SystemChatMessagePromptTemplate.fromTemplate(systemPrompt),
-      HumanChatMessagePromptTemplate.fromTemplate('{code_request}'),
-    ]);
-
-    // 5. Define the final chain
-    final model = ChatOpenAI(
-      apiKey: openaiApiKey,
-      defaultOptions: ChatOpenAIOptions(
-        model: 'gpt-4o-mini',
-        temperature: 0,
-        responseFormat: ChatOpenAIResponseFormat.jsonObject,
-      ),
-    );
-    final outputParser = JsonOutputParser<ChatResult>();
-    final chain = setupAndRetrieval
-        .pipe(promptTemplate)
-        .pipe(model)
-        .pipe(outputParser);
-
-    // 6. Run the pipeline
-    final res = await chain.invoke(codeRequest);
-
-    // 7. Write the samples to the file
-    writeSamplePartialDart(projectDir, getFileName(), res['code']);
-    return res['code'];
-  }
 }
 
 class CollectionSampleGenerator extends SampleGenerator {
@@ -150,14 +77,19 @@ class CollectionSampleGenerator extends SampleGenerator {
 
   @override
   Future<String> generateSampleData() async {
-    return await _generateSampleDataLLM(
+    return await generateSampleDataLLM(
       'Please produce 10 samples of the ${getRecordName()} objects.',
+      getFileName(),
+      getRecordName(),
+      projectDir,
+      openaiApiKey,
+      overwrite,
     );
   }
 
   // TODO: Needs to be updated to return the correct object
   String generateGetRecord() {
-    return '@override\n  Future<${getRecordName()}> getRecord(String id) async { return await samples[0]; }\n';
+    return '@override\n  Future<${getRecordName()}> getRecord(String id) async { return samples[0]; }\n';
   }
 
   // TODO: Implement Filters
@@ -165,7 +97,7 @@ class CollectionSampleGenerator extends SampleGenerator {
   Future<String> generateSampleClass() async {
     String res = 'class Sample$dataSourceName extends $dataSourceName {\n';
     res += '  ${await generateSampleData()}\n';
-    '  @override\n';
+    res += '  @override\n';
     res +=
         '  Future<List<${getRecordName()}>> getCollection(${getRequestName()} request) async {\n';
     res += '  return samples;\n';
@@ -201,8 +133,13 @@ class ListSampleGenerator extends SampleGenerator {
 
   @override
   Future<String> generateSampleData() async {
-    return await _generateSampleDataLLM(
+    return await generateSampleDataLLM(
       'Please produce 10 samples of the ${getRecordName()} objects.',
+      getFileName(),
+      getRecordName(),
+      projectDir,
+      openaiApiKey,
+      overwrite,
     );
   }
 
